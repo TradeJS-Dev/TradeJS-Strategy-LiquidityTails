4b55593aa47a47e40f2247db639abded874ab0c6
aleksnick <nikolaev@iprojects.ru>
2026-08-20 15:51:20 +0300

    feat: validate strategy configuration

--- src/LiquidityTails/strategy.ts
+++ src/LiquidityTails/strategy.ts
@@ -1,11 +1,16 @@
-import type { StrategyRegistryEntry } from "@tradejs/types";
+import { createStrategyConfigParser } from "@tradejs/strategy-kit/config";
+import type { ValidatedStrategyRegistryEntry } from "@tradejs/strategy-kit/config";
 import { config as DEFAULT_CONFIG, LiquidityTailsConfig } from "./config";
 import { createLiquidityTailsCore } from "./core";
 import { liquidityTailsManifest } from "./manifest";
 
-export const LiquidityTailsStrategyDefinition: StrategyRegistryEntry<LiquidityTailsConfig> =
+export const LiquidityTailsStrategyDefinition: ValidatedStrategyRegistryEntry<LiquidityTailsConfig> =
   {
     defaults: DEFAULT_CONFIG,
+    parseConfig: createStrategyConfigParser({
+      strategyName: "LiquidityTails",
+      defaults: DEFAULT_CONFIG,
+    }),
     createCore: createLiquidityTailsCore,
     manifest: liquidityTailsManifest,
   };
